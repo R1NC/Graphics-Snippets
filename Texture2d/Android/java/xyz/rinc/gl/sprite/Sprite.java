affeead82ffbb9d3105e38c5e5a7ff3ad3ff5621
rincliu <rincliu@tencent.com>
2018-08-10 12:58:26 +0800

    [Android][Texture2d] Modularize Sprite code;

--- Texture2d/Android/java/xyz/rinc/gl/sprite/Sprite.java
+++ Texture2d/Android/java/xyz/rinc/gl/sprite/Sprite.java
@@ -48,19 +48,19 @@ public class Sprite {
     private static final short[] DRAW_ORDER = {0, 1, 2, 0, 3, 2};
 
     // Position the eye behind the origin
-    private static final float LOOK_EYE_X = 0.0f;
-    private static final float LOOK_EYE_Y = 0.0f;
-    private static final float LOOK_EYE_Z = 3f;
+    private static final float CAMERA_EYE_X = 0.0f;
+    private static final float CAMERA_EYE_Y = 0.0f;
+    private static final float CAMERA_EYE_Z = 3f;
 
     // We are looking toward the distance
-    private static final float LOOK_CENTER_X = 0.0f;
-    private static final float LOOK_CENTER_Y = 0.0f;
-    private static final float LOOK_CENTER_Z = 0.0f;
+    private static final float CAMERA_CENTER_X = 0.0f;
+    private static final float CAMERA_CENTER_Y = 0.0f;
+    private static final float CAMERA_CENTER_Z = 0.0f;
 
     // This is where our head would be pointing were we holding the camera.
-    private static final float LOOK_UP_X = 0.0f;
-    private static final float LOOK_UP_Y = 1.0f;
-    private static final float LOOK_UP_Z = 0.0f;
+    private static final float CAMERA_UP_X = 0.0f;
+    private static final float CAMERA_UP_Y = 1.0f;
+    private static final float CAMERA_UP_Z = 0.0f;
 
     private FloatBuffer vertexBuffer;
     private FloatBuffer textureBuffer;
@@ -90,28 +90,13 @@ public class Sprite {
     void onSurfaceCreated() {
         loadShader();
         prepareBuffers();
-
-        // Set the view matrix. This matrix can be said to represent the camera position.
-        // NOTE: In OpenGL 1, a ModelView matrix is used, which is a combination of a model and
-        // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
-        Matrix.setLookAtM(cameraMatrix, 0, LOOK_EYE_X, LOOK_EYE_Y, LOOK_EYE_Z, LOOK_CENTER_X, LOOK_CENTER_Y, LOOK_CENTER_Z, LOOK_UP_X, LOOK_UP_Y, LOOK_UP_Z);
+        setCameraMatrix();
     }
 
     void onSurfaceChanged(int width, int height) {
         viewWidth = width;
         viewHeight = height;
-
-        // Create a new perspective projection matrix. The height will stay the same
-        // while the width will vary as per aspect ratio.
-        final float ratio = (float) width / height;
-        final float left = -ratio;
-        final float right = ratio;
-        final float bottom = -1.0f;
-        final float top = 1.0f;
-        final float near = 3.0f;
-        final float far = 7.0f;
-
-        Matrix.frustumM(projectionMatrix, 0, left, right, bottom, top, near, far);
+        updateProjectionMatrix(width, height);
     }
 
     void onDrawFrame() {
@@ -120,7 +105,11 @@ public class Sprite {
             imgHeight = bitmap.getHeight();
             GLUtil.setBitmap2Texture2d(0, bitmap);
         }
-        updateMatrices();
+
+        updateModelMatrix();
+
+        updateMatrices2Shader();
+
         drawElements();
     }
 
@@ -132,18 +121,14 @@ public class Sprite {
     }
 
     private void loadShader() {
-        //Load shader program
         program = GLUtil.loadShader(VERTEX_SHADER_CODE, FRAGMENT_SHADER_CODE);
         if (program > 0) {
             GLES20.glUseProgram(program);
-            //Prepare attributes and texture handles
             locPosition = GLES20.glGetAttribLocation(program, "a_Position");
             locTextureCoordinate = GLES20.glGetAttribLocation(program, "a_TexCoordinate");
             locProjectionMatrix = GLES20.glGetUniformLocation(program, "u_projectionMatrix");
             locCameraMatrix = GLES20.glGetUniformLocation(program, "u_cameraMatrix");
             locModelMatrix = GLES20.glGetUniformLocation(program, "u_modelMatrix");
-
-            // Generate texture2d and set parameters
             textureHandles = GLUtil.genTexture2d(1);
         }
     }
@@ -154,7 +139,27 @@ public class Sprite {
         drawOrderBuffer = GLUtil.prepareShortBuffer(DRAW_ORDER);
     }
 
-    private void updateMatrices() {
+    private void setCameraMatrix() {
+        // Set the camera matrix. This matrix can be said to represent the camera position.
+        // NOTE: In OpenGL 1, a ModelView matrix is used, which is a combination of a model and
+        // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
+        Matrix.setLookAtM(cameraMatrix, 0, CAMERA_EYE_X, CAMERA_EYE_Y, CAMERA_EYE_Z, CAMERA_CENTER_X, CAMERA_CENTER_Y, CAMERA_CENTER_Z, CAMERA_UP_X, CAMERA_UP_Y, CAMERA_UP_Z);
+    }
+
+    private void updateProjectionMatrix(int width, int height) {
+        // Create a new perspective projection matrix. The height will stay the same
+        // while the width will vary as per aspect ratio.
+        final float ratio = (float) width / height;
+        final float left = -ratio;
+        final float right = ratio;
+        final float bottom = -1.0f;
+        final float top = 1.0f;
+        final float nearZ = 3.0f;
+        final float farZ = 7.0f;
+        Matrix.frustumM(projectionMatrix, 0, left, right, bottom, top, nearZ, farZ);
+    }
+
+    private void updateModelMatrix() {
         Matrix.setIdentityM(modelMatrix, 0);
         Matrix.translateM(modelMatrix, 0, transX, transY, 0);
         Matrix.rotateM(modelMatrix, 0, angle, 0, 0, -1.0f);
@@ -164,7 +169,9 @@ public class Sprite {
             float baseScaleY = (float)imgHeight / targetSize;
             Matrix.scaleM(modelMatrix, 0, baseScaleX * scale, baseScaleY * scale, 1.0f);
         }
+    }
 
+    private void updateMatrices2Shader() {
         GLES20.glUniformMatrix4fv(locProjectionMatrix, 1, false, projectionMatrix, 0);
         GLES20.glUniformMatrix4fv(locCameraMatrix, 1, false, cameraMatrix, 0);
         GLES20.glUniformMatrix4fv(locModelMatrix, 1, false, modelMatrix, 0);
