4a86b02640e9a98b366ec7a98d782645309a8e1a
Rinc <i@rinc.xyz>
2022-10-31 16:06:38 +0800

    Update GLUtil.java

--- Texture2d/Android/GLES/java/xyz/rinc/gl/sprite/GLUtil.java
+++ Texture2d/Android/GLES/java/xyz/rinc/gl/sprite/GLUtil.java
@@ -85,10 +85,14 @@ public class GLUtil {
     }
 
     // [GL_TEXTURE0,GL_TEXTURE31]
-    public static void setBitmap2Texture2d(int texture2dIndex, Bitmap bitmap) {
+    public static void setBitmap2Texture2d(int texture2dIndex, Bitmap bitmap, boolean reuse) {
         if (bitmap == null || bitmap.isRecycled() || texture2dIndex < 0 || texture2dIndex > 31) return;
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0 + texture2dIndex);
-        GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0);
+        if (reuse) {
+            GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0);
+        } else {
+            GLUtils.texSubImage2D(GL_TEXTURE_2D, 0, 0, 0, bitmap);
+        }
         bitmap.recycle();
     }
 
