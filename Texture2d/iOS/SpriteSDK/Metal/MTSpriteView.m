6a89edf77f368f55946ef0629c4f175d3d2f15c5
Rinc <i@rinc.xyz>
2023-02-07 10:52:06 +0800

    Update MTSpriteView.m

--- Texture2d/iOS/SpriteSDK/Metal/MTSpriteView.m
+++ Texture2d/iOS/SpriteSDK/Metal/MTSpriteView.m
@@ -15,16 +15,19 @@
     return [CAMetalLayer class];
 }
 
+-(CAMetalLayer*)metalLayer {
+    return (CAMetalLayer*)self.layer;
+}
+
 -(instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         // An abstraction of GPU. Used to create buffers, textures, function libraries..
         _device = MTLCreateSystemDefaultDevice();
         
         // CAMetalLayer is a subclass of CALayer that knows how to display the contents of a Metal framebuffer.
-        CAMetalLayer *metalLayer = (CAMetalLayer*)self.layer;
-        metalLayer.opaque = NO;// Make layer transparent
-        metalLayer.device = _device;
-        metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+        self.metalLayer.opaque = NO;// Make layer transparent
+        self.metalLayer.device = _device;
+        self.metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
     }
     return self;
 }
@@ -35,7 +38,7 @@
     for (MTSprite* sprite in self.sprites) {
         // In order to draw into the Metal layer, we first need to get a ‘drawable’ from the layer.
         // The drawable object manages a set of textures that are appropriate for rendering into.
-        [sprite renderDrawable:[(CAMetalLayer*)self.layer nextDrawable] inRect:rect];
+        [sprite renderDrawable:[self.metalLayer nextDrawable] inRect:rect];
     }
 }
 
