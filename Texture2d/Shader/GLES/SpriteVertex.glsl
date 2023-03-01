432885336f7ba953ebd26ecd084ae1a3752fd9c5
Rinc <i@rinc.xyz>
2023-03-01 08:07:29 +0800

    Update SpriteVertex.glsl

--- Texture2d/Shader/GLES/SpriteVertex.glsl
+++ Texture2d/Shader/GLES/SpriteVertex.glsl
@@ -1,8 +1,8 @@
 attribute vec4 a_Position;
 attribute vec2 a_TextureCoordinate;
 varying float v_ETC;
-varying vec2 v_TextureCoordinate;
-varying vec2 v_AlphaCoordinate;
+varying vec2 v_rgbCoordinate;
+varying vec2 v_alphaCoordinate;
 uniform mat4 u_projectionMatrix;
 uniform mat4 u_cameraMatrix;
 uniform mat4 u_modelMatrix;
@@ -12,9 +12,9 @@ void main() {
     gl_Position = u_projectionMatrix * u_cameraMatrix * u_modelMatrix * a_Position;
     v_ETC = u_ETC;
     if (v_ETC != 0.0) {
-        v_TextureCoordinate = a_TextureCoordinate * vec2(1.0, 0.5);
-        v_AlphaCoordinate = v_TextureCoordinate + vec2(0.0, 0.5);
+        v_rgbCoordinate = a_TextureCoordinate * vec2(1.0, 0.5);
+        v_alphaCoordinate = v_rgbCoordinate + vec2(0.0, 0.5);
     } else {
-        v_TextureCoordinate = a_TextureCoordinate;
+        v_rgbCoordinate = a_TextureCoordinate;
     }
 }
