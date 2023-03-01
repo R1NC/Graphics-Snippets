576f1faaeddc607af8d5b17fa2b5d49bdd488dba
Rinc <i@rinc.xyz>
2023-03-01 08:08:22 +0800

    Update SpriteFragment.glsl

--- Texture2d/Shader/GLES/SpriteFragment.glsl
+++ Texture2d/Shader/GLES/SpriteFragment.glsl
@@ -1,15 +1,15 @@
 precision mediump float;
 uniform sampler2D u_Texture;
 varying float v_ETC;
-varying vec2 v_TextureCoordinate;
-varying vec2 v_AlphaCoordinate;
+varying vec2 v_rgbCoordinate;
+varying vec2 v_alphaCoordinate;
 
 void main() {
     if (v_ETC != 0.0) {
-        vec4 color = texture2D(u_Texture, v_TextureCoordinate);
-        color.a = texture2D(u_Texture, v_AlphaCoordinate).r;
+        vec4 color = texture2D(u_Texture, v_rgbCoordinate);
+        color.a = texture2D(u_Texture, v_alphaCoordinate).r;
         gl_FragColor = color;
     } else {
-        gl_FragColor = texture2D(u_Texture, v_TextureCoordinate);
+        gl_FragColor = texture2D(u_Texture, v_rgbCoordinate);
     }
 }
